e39843ffafa979f994e64cf45ddccc1dea8dabbf
khaledxab <khaledxab@gmail.com>
2022-12-28 14:05:03 +0100

    sections

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './Nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBookAlt} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {RiContactsLine} from 'react-icons/ri'
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBookAlt/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><RiContactsLine/></a>
+    </nav>
   )
 }
 
